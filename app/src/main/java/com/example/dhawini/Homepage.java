06496cf11427afc0476e56c23bd834fed9b66ce6
eya-bj <eya.benjemia2004@gmail.com>
2025-11-21 09:40:33 +0100

    landing_page_complete

--- app/src/main/java/com/example/dhawini/Homepage.java
+++ app/src/main/java/com/example/dhawini/Homepage.java
@@ -1,5 +1,6 @@
 package com.example.dhawini;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,19 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvWelcome), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ivLogo), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        findViewById(R.id.btnClient).setOnClickListener(v ->
+                startActivity(new Intent(this, QuickAssessementActivity.class)));
+
+        findViewById(R.id.btnCompany).setOnClickListener(v ->
+                startActivity(new Intent(this, CompanyLoginActivity.class)));
+
+        findViewById(R.id.btnAdmin).setOnClickListener(v ->
+                startActivity(new Intent(this, AdminLoginActivity.class)));
     }
+
 }
